3575350965d70ab79bc49f2d32c05cdd6914ebab
sattbuttar <45004111+sattbuttar@users.noreply.github.com>
2018-12-01 23:35:38 +0530

    Add files via upload

---
+++ SatwinderSingh/A3SatwinderSinghP1/A3SatwinderSinghP1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3SatwinderSinghP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Press 1 to display a list of even numbers starting from 0 ");
+                Console.WriteLine("Press 2 to display a sequence of perfect squares starting from 1 ");
+                Console.WriteLine("Press 3 to exit ");
+                double l = double.Parse(Console.ReadLine());
+
+                if (l == 1)
+                {
+                    Console.WriteLine(" Type the number of even numbers you want as result ");
+                    double m= double.Parse(Console.ReadLine());
+
+                    for (int n = 2; n < 2 *m; n=n+2)
+                    {
+                        Console.WriteLine(n);
+                    }
+
+                }
+
+                
+                
+                    if (l == 2)
+                    {
+                        Console.WriteLine("1");
+                        do
+                        {
+
+                            Console.WriteLine(" Enter 1 to continue and 2 to stop ");
+                            char a = char.Parse(Console.ReadLine());
+
+                        } while (Console.ReadLine() != "1");
+                        {
+                            int d = 1;
+                            int f = (d + 1) * (d + 1);
+                            Console.WriteLine(f);
+
+                        }
+                    }
+
+                    if(l==3)
+                    {
+                            
+                        
+
+                    }
+
+
+
+            }
+            catch(FormatException o)
+            {
+                Console.WriteLine("ENTER VALID VALUE");
+
+            }
+
+            finally
+            {
+
+            }
+               
+           
+        }
+    }
+}
---
+++ SatwinderSingh/A3SatwinderSinghP2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3SatwinderSinghP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine(" Enter the first number ");
+            double one = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Enter the second number ");
+            double two = double.Parse(Console.ReadLine());
+
+
+            try { 
+
+            Console.WriteLine(" Enter 6 for addition, 7 for suctraction, 8 for multiplication and 9 for division ");
+            int cal = int.Parse(Console.ReadLine());
+
+              if (cal == 6)
+              {
+                
+                double r = one + two;
+                Console.WriteLine("result = " + r);
+
+              }
+
+
+              if (cal == 7)
+              {
+
+                double r = one - two;
+                Console.WriteLine("result = " + r);
+
+              }
+
+              if (cal == 8)
+              {
+
+                double r = one * two;
+                Console.WriteLine("result = " + r);
+
+              }
+
+              if (cal == 9)
+              {
+
+                double r = one / two;
+                Console.WriteLine("result = " + r);
+
+              }
+
+            }
+            catch(FormatException x)
+            {
+                Console.WriteLine("Enter valid value for the operation selection");
+            }
+            finally
+            {
+
+            }
+
+        }
+    }
+}
